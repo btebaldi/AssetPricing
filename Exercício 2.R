04622c0f8430e627fae3cc1a4fdbeb779eb0bb28
Teo <btebaldi@deskpaper.com>
2019-09-27 20:57:49 -0300

    hansen

--- Exercício 2.R
+++ Exercício 2.R
@@ -3,50 +3,61 @@ rm(list = ls())
 
 # biblioteca utilizadas
 library(gmm)
+library(readxl)
+library(dplyr)
 
-# Leitura de base de dados #
-cbapmewrdata  = read.table("dataset/cbapmewrdata.dat")
-cbapmewrinstr = read.table("dataset/cbapmewrinstr.dat")
-cbapmvwrdata  = read.table("dataset/cbapmvwrdata.dat")
-cbapmvwrinstr = read.table("dataset/cbapmvwrinstr.dat")
+EW.dados <- read_excel("dataset/dadosHansenSingleton.xlsx", sheet = "EW.data")
+VW.dados <- read_excel("dataset/dadosHansenSingleton.xlsx", sheet = "VW.data")
 
-head(cbapmewrdata)
-head(cbapmvwrdata)
-head(cbapmewrinstr)
-dim(cbapmewrdata)
+EW.dados = mutate(EW.dados, "Ct_1" = lag(Ct, 1), "Rt_1" = lag(Rt, 1))
+EW.dados = mutate(EW.dados, "Ct_2" = lag(Ct, 2), "Rt_2" = lag(Rt, 2))
+EW.dados = mutate(EW.dados, "Ct_3" = lag(Ct, 3), "Rt_3" = lag(Rt, 3))
+EW.dados = mutate(EW.dados, "Ct_4" = lag(Ct, 4), "Rt_4" = lag(Rt, 4))
+EW.dados = mutate(EW.dados, "Ct_5" = lag(Ct, 5), "Rt_5" = lag(Rt, 5))
+EW.dados = mutate(EW.dados, "Ct_6" = lag(Ct, 6), "Rt_6" = lag(Rt, 6))
 
-# atribuindo nomes (verificar! Nomes determinados olhando as medias) (palpite!) 
-names(cbapmewrdata) = c("C" , "R")
-names(cbapmvwrdata) = c("C" , "R")
+VW.dados = mutate(VW.dados, "Ct_1" = lag(Ct, 1), "Rt_1" = lag(Rt, 1))
+VW.dados = mutate(VW.dados, "Ct_2" = lag(Ct, 2), "Rt_2" = lag(Rt, 2))
+VW.dados = mutate(VW.dados, "Ct_3" = lag(Ct, 3), "Rt_3" = lag(Rt, 3))
+VW.dados = mutate(VW.dados, "Ct_4" = lag(Ct, 4), "Rt_4" = lag(Rt, 4))
+VW.dados = mutate(VW.dados, "Ct_5" = lag(Ct, 5), "Rt_5" = lag(Rt, 5))
+VW.dados = mutate(VW.dados, "Ct_6" = lag(Ct, 6), "Rt_6" = lag(Rt, 6))
 
-names(cbapmewrinstr) = c("Const" , "C-1", "C-2" , "R-1" , "R-2")
-names(cbapmvwrinstr) = c("Const" , "C-1", "C-2" , "R-1" , "R-2")
+EW.dados$Const = 1
+VW.dados$Const = 1
 
-# analisando a base
-head(cbind(cbapmewrdata,cbapmewrinstr)[,c("Const" , "C" ,"C-1", "C-2" , "R" , "R-1" , "R-2")], 5)
-head(cbind(cbapmvwrdata,cbapmvwrinstr)[,c("Const" , "C" ,"C-1", "C-2" , "R" , "R-1" , "R-2")], 5)
+head(EW.dados)
+head(VW.dados)
 
-# note que as bases sao mensais!! 
-sapply(cbapmewrdata , mean)**12
-sapply(cbapmewrinstr , mean)**12
+# # analisando a base
+# head(cbind(cbapmewrdata,cbapmewrinstr)[,c("Const" , "C" ,"C-1", "C-2" , "R" , "R-1" , "R-2")], 5)
+# head(cbind(cbapmvwrdata,cbapmvwrinstr)[,c("Const" , "C" ,"C-1", "C-2" , "R" , "R-1" , "R-2")], 5)
 
-sapply(cbapmvwrdata , mean)**12
-sapply(cbapmvwrinstr , mean)**12
-
-dim(cbapmvwrinstr)
-dim(cbapmvwrdata)
+# # note que as bases sao mensais!! 
+# sapply(cbapmewrdata , mean)**12
+# sapply(cbapmewrinstr , mean)**12
+# 
+# sapply(cbapmvwrdata , mean)**12
+# sapply(cbapmvwrinstr , mean)**12
+# 
+# dim(cbapmvwrinstr)
+# dim(cbapmvwrdata)
 
 # observacao 2: todas as bases tem mesmo numero de observacoes. OK!
 
 ################    Fim tratamento de bases   ##############
 
 ###### Contrucao do GMM ##########
-J = function(param , base){
+# A function of the form g(θ,x) and which returns a n \times q matrix with typical element g_i(θ,x_t)
+# for i=1,...q and t=1,...,n.
+# This matrix is then used to build the q sample moment conditions.
+GMatrix = function(param, base){
   beta  = param[1]
   gamma = param[2]
 
-  C = base$C
-  R = base$R
+  C = base$Ct
+  R = base$Rt
+  
   Z = base[,3:ncol(base)]
       
   g = (beta*(C^(gamma))*R-1)*(as.matrix(Z))
